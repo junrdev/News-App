ca167b425834e2fe3f664c405802303b1d595e5c
star <juniorprogrammer09@gmail.com>
2023-05-29 01:48:45 +0300

    modified view and functionality

--- app/src/main/java/com/junrdev/news/SingleNoteView.java
+++ app/src/main/java/com/junrdev/news/SingleNoteView.java
@@ -3,7 +3,9 @@ package com.junrdev.news;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +19,7 @@ import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.junrdev.news.databinding.ActivitySingleNoteViewBinding;
 
 import java.util.Objects;
@@ -25,11 +28,12 @@ public class SingleNoteView extends AppCompatActivity {
 
     private ActivitySingleNoteViewBinding binding;
 
-    private TextView singleNewsTitle, singleNewsDescription, singleNewsContent;
+    private TextView singleNewsTitle, singleNewsDescription, singleNewsContent, singleNewsAuthor, singleNewsDate;
 
-    private ProgressBar imageLoading;
 
     private ImageView imagePlaceHolder;
+
+    private ShapeableImageView openInBrowserImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +48,10 @@ public class SingleNoteView extends AppCompatActivity {
         singleNewsDescription = binding.singleNewsDescription;
         singleNewsContent = binding.singleNewsContent;
         imagePlaceHolder = binding.imagePlaceHolder;
+        singleNewsAuthor = binding.singleNewsAuthor;
+        singleNewsDate = binding.singleNewsDate;
 
-        imageLoading = binding.imageLoading;
-        imageLoading.setVisibility(View.VISIBLE);
+        openInBrowserImageView = binding.openInBrowserImageView;
 
         if(!getIntent().getStringExtra("title").isEmpty()){
 
@@ -61,14 +66,24 @@ public class SingleNoteView extends AppCompatActivity {
             singleNewsTitle.setText(title);
             singleNewsDescription.setText(description);
             singleNewsContent.setText(content);
+            singleNewsAuthor.setText(author);
+            singleNewsDate.setText(publication_date);
 
             //load image using glide
             runOnUiThread(()-> {
                 Glide.with(this).load(url_to_image).into(imagePlaceHolder);
-                imageLoading.setVisibility(View.GONE);
             });
 //            Toast.makeText(this, " url "+url_to_image, Toast.LENGTH_SHORT).show();
 
+
+            //openning the news link inside a browser
+            openInBrowserImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent _browser_intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(_browser_intent);
+                }
+            });
         }
 
 
